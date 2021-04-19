e3ed4bc1534c80736919658e7de4553a94c8b120
Felix <felix.boeer@mailbox.org>
2021-04-19 17:59:19 +0200

    extractDir working with version 2.x

--- src/test/java/com/example/demo/FlapdoodleMongoConfiguration.java
+++ src/test/java/com/example/demo/FlapdoodleMongoConfiguration.java
@@ -1,40 +1,36 @@
 package com.example.demo;
 
 import de.flapdoodle.embed.mongo.Command;
-import de.flapdoodle.embed.mongo.config.Defaults;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.ExtractedArtifactStoreBuilder;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.config.RuntimeConfig;
-import de.flapdoodle.embed.process.extract.DirectoryAndExecutableNaming;
-import de.flapdoodle.embed.process.extract.NoopTempNaming;
-import de.flapdoodle.embed.process.io.directories.Directory;
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import de.flapdoodle.embed.process.io.directories.FixedPath;
-
+import de.flapdoodle.embed.process.io.directories.IDirectory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 @Configuration
 public class FlapdoodleMongoConfiguration {
     private static final Command command = Command.MongoD;
-    private static final Directory extractStorePathRelative = new FixedPath("target/embedmongo");
+    private static final IDirectory extractStorePathRelative = new FixedPath("target/embedmongo");
     // private static final Directory extractStorePathAbsolute = new FixedPath("<absolute-path>/target/embedmongo");
 
     @Bean
-    public RuntimeConfig runtimeConfig() {
-        return Defaults.runtimeConfigFor(command)
-                       .artifactStore(Defaults.extractedArtifactStoreFor(command)
-                                              .withExtraction(DirectoryAndExecutableNaming.builder()
-                                                                                          .directory(extractStorePathRelative)
-                                                                                          .executableNaming(new NoopTempNaming())
-                                                                                          .build())
-                       )
-                       .build();
+    public IRuntimeConfig runtimeConfig() {
+        return new RuntimeConfigBuilder().defaults(command)
+                                         .artifactStore(new ExtractedArtifactStoreBuilder().defaults(command)
+                                                                                           .extractDir(extractStorePathRelative))
+                                         .build();
     }
 
     @Bean
-    public MongodConfig versionConfig() {
-        return MongodConfig.builder()
-                           .version(Version.V4_0_12)
-                           .build();
+    public IMongodConfig versionConfig() throws IOException {
+        return new MongodConfigBuilder().version(Version.V4_0_2)
+                                        .build();
     }
 }
